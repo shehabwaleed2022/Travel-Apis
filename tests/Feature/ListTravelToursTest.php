6f44d38cb3c27518af18274c30686bccffdcb3df
shehabwaleed2022 <shehabwaleed2010@gmail.com>
2023-09-18 11:27:44 +0300

    Convert some of tests to PEST

--- tests/Feature/ListTravelToursTest.php
+++ tests/Feature/ListTravelToursTest.php
@@ -1,54 +1,40 @@
 <?php
 
-namespace Tests\Feature;
-
 use App\Models\Tour;
 use App\Models\Travel;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
 
-class ListTravelToursTest extends TestCase
-{
-    use RefreshDatabase;
 
-    public function test_tours_list_correctly_for_travel_slug(): void
-    {
-        $travel = Travel::factory()->create();
+test('tours list correctly for travel slug', function () {
+    $travel = Travel::factory()->create();
         $tours = Tour::factory(15)->create(['travel_id' => $travel->id]);
 
-        $response = $this->get('/api/v1/travels/'.$travel->slug.'/tours');
+        $this->get('/api/v1/travels/'.$travel->slug.'/tours')
+        ->assertStatus(200)
+        ->assertJsonFragment(['id' => $tours->first()->id])
+        ->assertJsonCount(15, 'data')
+        ->assertJsonFragment(['travel_id' => $travel->id]);
+});
 
-        $response->assertStatus(200);
-        $response->assertJsonFragment(['id' => $tours->first()->id]);
-        $response->assertJsonCount(15, 'data');
-        $response->assertJsonFragment(['travel_id' => $travel->id]);
-    }
 
-    public function test_tours_list_filters(): void
-    {
-        $travel = Travel::factory()->create();
-        $tourWithPrice120 = Tour::factory()->create([
-            'travel_id' => $travel->id,
-            'price' => 120 * 100,
-        ]);
-        $tourWithPrice400 = Tour::factory()->create([
-            'travel_id' => $travel->id,
-            'price' => 400 * 100,
-        ]);
+test('tours list filters works correctly', function(){
+    $travel = Travel::factory()->create();
+    $tourWithPrice120 = Tour::factory()->create([
+        'travel_id' => $travel->id,
+        'price' => 120 * 100,
+    ]);
+    $tourWithPrice400 = Tour::factory()->create([
+        'travel_id' => $travel->id,
+        'price' => 400 * 100,
+    ]);
 
-        // Test price filter
-        $response = $this->get('/api/v1/travels/'.$travel->slug.'/tours?priceFrom=110&priceTo=300');
-        $response->assertStatus(200);
-        $response->assertJsonCount(1, 'data');
-        $response->assertJsonFragment(['id' => $tourWithPrice120->id]);
+    // Test price filter
+    $response = $this->get('/api/v1/travels/'.$travel->slug.'/tours?priceFrom=110&priceTo=300')
+    ->assertStatus(200)
+    ->assertJsonCount(1, 'data')
+    ->assertJsonFragment(['id' => $tourWithPrice120->id]);
 
-        $response = $this->get('/api/v1/travels/'.$travel->slug.'/tours?priceFrom=150&priceTo=500');
-        $response->assertStatus(200);
-        $response->assertJsonCount(1, 'data');
-        $response->assertJsonFragment(['id' => $tourWithPrice400->id]);
-        // Test date filter
-
-    }
-
-    // Test ordering filter
-}
+    $response = $this->get('/api/v1/travels/'.$travel->slug.'/tours?priceFrom=150&priceTo=500')
+    ->assertStatus(200)
+    ->assertJsonCount(1, 'data')
+    ->assertJsonFragment(['id' => $tourWithPrice400->id]);
+});
--- tests/Feature/ListTravelsTest.php
+++ tests/Feature/ListTravelsTest.php
@@ -1,23 +1,13 @@
 <?php
 
-namespace Tests\Feature;
-
 use App\Models\Travel;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
 
-class ListTravelsTest extends TestCase
-{
-    use RefreshDatabase;
 
-    public function test_public_travel_retireved_successfully(): void
-    {
-        Travel::factory(20)->create(['is_public' => true]);
-        Travel::factory(1)->create(['is_public' => false]);
+test('Travels listed successfully', function () {
+    Travel::factory(20)->create(['is_public' => true]);
+    Travel::factory(1)->create(['is_public' => false]);
 
-        $response = $this->get('api/v1/travels/public');
-
-        $response->assertStatus(200);
-        $response->assertJsonCount(20, 'data');
-    }
-}
+    $this->get('api/v1/travels/public')
+    ->assertStatus(200)
+    ->assertJsonCount(20, 'data');
+});
